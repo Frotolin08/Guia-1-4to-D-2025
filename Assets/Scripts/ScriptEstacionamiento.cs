c4cbf4721ebd460ba39265ead192650923dbf3f1
Frotolin <mathifogo@gmail.com>
2025-04-03 10:40:51 -0300

    ejercicio 10 terminado

---
+++ Assets/Scripts/ScriptEstacionamiento.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptEstacionamiento : MonoBehaviour
+{
+    public int horaActual;
+    bool estaAbierto;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        if(horaActual<0 || horaActual>24)
+        {
+
+            Debug.Log("Se ha ingresado una hora incorrecta");
+            return;
+        }
+        else if(horaActual >= 10 && horaActual<18)
+        {
+            estaAbierto = true;
+
+        }
+        else
+        {
+            estaAbierto = false;
+        }
+        Debug.Log("es " + estaAbierto + " que el estacionamiento esta abierto");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
